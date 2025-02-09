5abe68e9785f2948fede5c94aa185e4687a6e5a2
MichaelT-W23 <Michael.Totaro@icloud.com>
2025-02-08 23:29:00 -0500

    Router

---
+++ pages/user_posts.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>People List</title>
+</head>
+<body>
+
+    <h2 style="text-align: center;">SUPER COOL</h2>
+
+    <div class="button-container">
+      <a href="/">
+        <button>Go to Home</button>
+      </a>
+    </div>
+
+</body>
+
+<style>
+
+body {
+  font-family: Arial, sans-serif;
+}
+
+table {
+  width: 50%;
+  border-collapse: collapse;
+  margin: 20px auto;
+}
+
+th, td {
+  border: 1px solid black;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+
+.button-container {
+  text-align: center;
+  margin-top: 20px;
+}
+
+</style>
+
+</html>
