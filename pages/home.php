b2091f03c205e98d1e00e3a1b2d567f389cdceb6
MichaelT-W23 <Michael.Totaro@icloud.com>
2025-02-09 12:26:01 -0500

    Style changes

--- pages/home.php
+++ pages/home.php
@@ -103,7 +103,9 @@ h2 {
 }
 
 .card-header {
-  background-color: #f2f2f2;
+  /* background-color: #f2f2f2; */
+  background-color: purple;
+  color: white;
   padding: 15px;
   font-size: 18px;
   font-weight: bold;
