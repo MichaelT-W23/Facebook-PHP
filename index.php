c479d16fa4164ff14e3bc81e982c40eb20931d33
MichaelT-W23 <Michael.Totaro@icloud.com>
2025-02-11 12:10:30 -0500

    Fixed index.php

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 
-$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$request = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), "/");
 
 switch ($request) {
-    case '/':
+    case '':
+    case 'index.php':
         require 'pages/home.php';
         break;
-    case '/posts':
+    case 'posts':
         require 'pages/user_posts.php';
+        break;
+    default:
+        http_response_code(404);
+        echo "404 - Page Not Found";
         break;
 }
 
 ?>
+
