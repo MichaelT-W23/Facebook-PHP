7fecd08d125db187d9b927507118141750d68729
MichaelT-W23 <Michael.Totaro@icloud.com>
2025-02-11 13:52:05 -0500

    Data init.sql

---
+++ data/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    school VARCHAR(255),
+    birthday DATE,
+    hometown VARCHAR(255)
+);
+
+CREATE TABLE posts (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    likes INT DEFAULT 0 CHECK (likes >= 0),
+    date_posted DATE,
+    content TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
